07dd617f6df0fcb784529033287980eb8b11949b
heresson <heresson@mppa.mp.br>
2022-08-23 20:34:48 -0300

    ajustes no componente formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="box">
     <div class="columns">
-      <div 
-        class="column is-8"
-        role="form"
-        aria-label="Formulário para criação de uma nova tarefa"
-      >
-        <input 
-          type="text"
-          class="input"
-          placeholder="Qual tarefa você deseja iniciar"
-        />
+      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar" />
       </div>
       <div class="column">
         <div class="is-flex is-align-items-center is-justify-content-space-between">
-                  <section>
-          <strong>00:00:00</strong>
-        </section>
+          <section>
+            <strong>00:00:00</strong>
+          </section>
 
           <button class="button">
             <span class="icon">
@@ -26,11 +18,11 @@
           </button>
 
           <button class="button">
-          <span class="icon">
-            <i class="fas fa-stop"></i>
-          </span>
-          <span>stop</span>
-        </button>        
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
         </div>
 
       </div>
@@ -39,7 +31,7 @@
 </template>
 
 <script lan="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'Formulario',
@@ -47,5 +39,4 @@ export default defineComponent({
 </script>
 
 <style>
-
 </style>
