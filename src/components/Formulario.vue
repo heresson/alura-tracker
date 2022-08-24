6170963ebb3851e4e3a57a4e3e3779daa02a11bb
Heresson Mendes <heresson@mppa.mp.br>
2022-08-24 15:52:01 -0300

    isolando componenten Cronometro

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -6,10 +6,9 @@
       </div>
       <div class="column">
         <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
-
+          
+          <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+          
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -32,29 +31,30 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Cronometro from './Cronometro.vue'
 
 export default defineComponent({
   name: 'Formulario',
+  components: {
+    Cronometro,
+  },
   data() {
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
     }
   },
-  computed: {
-    tempoDecorrido () : string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11,19)
-    }
-  },
+
   methods: {
     iniciar() {
-
+      console.log("Iniciando...")
       this.cronometro = setInterval(() => {
         this.tempoEmSegundos += 1
       }, 1000);
       
     },
     finalizar() {
+      console.log("Finalizando...")
       clearInterval(this.cronometro);
     }
 
