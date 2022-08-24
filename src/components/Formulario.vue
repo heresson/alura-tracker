50c51b1523de940968443fadd16ccd1fd29a8417
heresson <heresson@mppa.mp.br>
2022-08-24 18:33:26 -0300

    Isolando o componente Temporizador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -4,61 +4,20 @@
       <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar" />
       </div>
-      <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
-          
-          <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-          
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
-
-      </div>
+      <Temporizador />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Cronometro from './Cronometro.vue'
+import Temporizador from './Temporizador.vue'
 
 export default defineComponent({
   name: 'Formulario',
   components: {
-    Cronometro,
-  },
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0,
-    }
-  },
-
-  methods: {
-    iniciar() {
-      console.log("Iniciando...")
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1
-      }, 1000);
-      
-    },
-    finalizar() {
-      console.log("Finalizando...")
-      clearInterval(this.cronometro);
-    }
-
-  },
+    Temporizador
+},
 })
 </script>
 
