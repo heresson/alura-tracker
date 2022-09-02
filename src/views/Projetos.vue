960cae7c1ef54b4d46b5dd7ad0393de257306b59
Heresson Mendes <heresson@mppa.mp.br>
2022-09-02 12:03:09 -0300

    formulario de projetos

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,15 +1,68 @@
 <template>
-    <div>
-        <h1>AAA | PROJETOS</h1>
-    </div>
+     <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvarProjeto">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeProjeto"
+          id="nomeDoProjet"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in listaProjetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
 
 import { defineComponent } from "vue";
+import IProjeto from '../interfaces/IProjeto'
 
 export default defineComponent({
     name: 'Projetos',
+    data() {
+        return {
+            nomeProjeto: '',
+            listaProjetos: [] as IProjeto[],
+        }
+    },
+    methods: {
+        salvarProjeto() {
+            const projeto: IProjeto = {
+                nome: this.nomeProjeto,
+                id: new Date().toISOString()
+            }
+
+            this.listaProjetos.push(projeto)
+            this.nomeProjeto = ''
+
+        }
+    }
 })
 
 </script>
+
+<style scoped>
+    .projetos {
+      padding: 1.25rem;
+    }
+</style>
