5da310283a7ebb982868a9142c8eb777a68ecaa3
Heresson Mendes <heresson@mppa.mp.br>
2022-09-05 13:46:32 -0300

    ajuste na lista de tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -24,7 +24,7 @@
         </div>
       </div>
       <div class="column">
-        <Temporizador @aoFinalizarTarefa="salvarTarefa"/>
+        <Temporizador @aoTemporizadorFinalizado="salvarTarefa"/>
       </div>
     </div>
   </div>
