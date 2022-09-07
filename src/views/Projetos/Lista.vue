f1e680d0ca6d2f2761d1635adc6c1ba897dc8b6e
heresson <heresson@mppa.mp.br>
2022-09-07 19:10:04 -0300

    exclusao de projetos e refatoração da store

--- src/views/Projetos/Lista.vue
+++ src/views/Projetos/Lista.vue
@@ -24,6 +24,11 @@
                 <i class="fas fa-pencil-alt"></i>
               </span>
             </router-link>
+            <button class="button ml-2 is-danger" @click="excluir(projeto.id)">
+              <span class="icon is-small">
+                <i class="fas fa-trash"></i>
+              </span>
+            </button>
           </td>
         </tr>
       </tbody>
@@ -33,16 +38,23 @@
 
 <script lang="ts">
 import { useStore } from "@/store";
+import { EXCLUI_PROJETO } from "@/store/metodos-projetos";
 import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   name: "Lista",
-  setup () {
+  setup() {
     const store = useStore()
     return {
-      projetos: computed(() => store.state.projetos)
+      projetos: computed(() => store.state.projetos),
+      store,
     }
-  }
+  },
+  methods: {
+    excluir(id: string) {
+      this.store.commit(EXCLUI_PROJETO,id)
+    },
+  },
 });
 </script>
 
